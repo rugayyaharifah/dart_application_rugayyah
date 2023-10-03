82c0d27d4c1241d45cea81ce3ccc64404533e48a
rugayyaharifah <rugayyaharifah@gmail.com>
2023-10-03 12:35:45 +0700

    Initial Project

---
+++ test/dart_application_rugayyah_test.dart
@@ -0,0 +1,8 @@
+import 'package:dart_application_rugayyah/dart_application_rugayyah.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
