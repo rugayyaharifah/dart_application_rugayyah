7d6e43007ee57f31ca13ab0270d7e506fa4e4981
rugayyaharifah <rugayyaharifah@gmail.com>
2023-10-03 12:46:15 +0700

    Change to hello everyone

--- bin/dart_application_rugayyah.dart
+++ bin/dart_application_rugayyah.dart
@@ -1,5 +1,5 @@
 import 'package:dart_application_rugayyah/dart_application_rugayyah.dart' as dart_application_rugayyah;
 
 void main(List<String> arguments) {
-  print('Hello world: ${dart_application_rugayyah.calculate()}!');
+  print('Hello everyone');
 }
